a6fb2a674933557f466060445190c24b03b97c25
Siddharth Gelera <sidgroks@gmail.com>
2018-03-04 23:43:02 +0530

    work boiler added, working hash routing function

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-function activate(e){
-	var value = e.innerHTML.toLowerCase();
-	var section = document.getElementById(value);
+const body = document.querySelector('body');
+body.onload = checkHashAndActivateSection;
+
+function checkHashAndActivateSection(e){
+	const value = window.location.hash.split('/')[1];
+	activate(e,value);
+}
+
+function activate(e,value){
+	if(!value){
+		value = e.innerHTML.toLowerCase();
+	}
+	var section=document.getElementById(value);
 	var sections = document.getElementsByTagName('section');
 	var buttons = document.querySelectorAll('.nav-btn');
 
@@ -16,5 +26,21 @@ function activate(e){
 	section.classList.add('display-flex');
 	section.offsetWidth;
 	section.classList.add('active');
-	e.classList.add('active-btn')
+	changeButtonToActive({activeClass:'active-btn',menuText:value});
+	window.location='/#/'+ (value || e.innerHTML.toLowerCase());
+}
+
+function goTo(url){
+	window.location=url;
+}
+
+function changeButtonToActive(opts){
+	var activeClassName = opts.activeClass;
+	var menuText = opts.menuText;
+	var navButtons = document.querySelectorAll('.nav-btn');
+	navButtons.forEach(function(button){
+		if(button.innerHTML && button.innerHTML.toLowerCase() === menuText.toLowerCase()){
+			button.classList.add(activeClassName);
+		}
+	})
 }
