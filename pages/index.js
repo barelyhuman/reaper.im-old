fadd551b26d8dfce7473e6cb98e452113e0cc83b
Siddharth Gelera <ahoy@barelyhuman.dev>
2020-04-12 15:55:28 +0530

    update logos

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ function Index(){
   </Head>
   <div className="container">
     <div className="margin-md flex just-center flex-col align-center">
-      <img src="/logo.png" height={100} width={100} alt="logo"/>
+      <img src="/logo.png" height={100} alt="logo"/>
       <a href="/" className="action-link">
         <span className="dark-gray font-size-50 text-center flex just-center">
           Siddharth Gelera
