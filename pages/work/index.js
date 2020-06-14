adb43b3ab0fbd38c3ddf624197549745b252087a
Siddharth Gelera <ahoy@barelyhuman.dev>
2020-06-14 17:02:23 +0530

    clearnup

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,77 +1,77 @@
 import Layout from '../../components/Layout';
 import Router from 'next/router'
 
-function Work(){
+function Work() {
   return <Layout>
-  <div className="container">
-    <ul className="card-border min-width-150-px">
-    <li>
-        <a target="_blank" href="https://github.com/barelyhuman/route/">
-          Route - File Tree based API Routes for NodeJs
+    <div className="container">
+      <ul className="card-border min-width-150-px">
+        <li>
+          <a target="_blank" href="https://github.com/barelyhuman/route/">
+            Route - File Tree based API Routes for NodeJs
         </a>
-      </li>
-    <li>
-        <a target="_blank" href="https://tillwhen.barelyhuman.dev/">
-          TillWhen - Just a time tracker
+        </li>
+        <li>
+          <a target="_blank" href="https://tillwhen.barelyhuman.dev/">
+            TillWhen - Just a time tracker
         </a>
-      </li>
+        </li>
 
-    <li>
-        <a target="_blank" href="https://mnmlcss.js.org/">
-          Mnml.css - Minimal CSS Resets
+        <li>
+          <a target="_blank" href="https://mnmlcss.js.org/">
+            Mnml.css - Minimal CSS Resets
         </a>
-      </li>
-      <li>
-        <a target="_blank" href="https://monotes.barelyhuman.dev/home">
-          Monotes - Markdown Editor
+        </li>
+        <li>
+          <a target="_blank" href="https://monotes.barelyhuman.dev/home">
+            Monotes - Markdown Editor
         </a>
-      </li>
-      <li>
-        <a target="_blank" href="https://hireme.barelyhuman.dev/">
-          HireMe - Job Network
+        </li>
+        <li>
+          <a target="_blank" href="https://hireme.barelyhuman.dev/">
+            HireMe - Job Network
         </a>
-      </li>
-      <li>
-        <a target="_blank" href="https://corona.siddharthgelera.com">
-          Covid - 19 Tracker
+        </li>
+        <li>
+          <a target="_blank" href="https://corona.siddharthgelera.com">
+            Covid - 19 Tracker
         </a>
-      </li>
-      <li>
-        <a target="_blank" href="https://orion.barelyhuman.dev">
-          Orion - Music Player UI
+        </li>
+        <li>
+          <a target="_blank" href="https://orion.barelyhuman.dev">
+            Orion - Music Player UI
         </a>
-      </li>
-      <li>
-        <a target="_blank" href="https://washed.siddharthgelera.com">
-          Washed - Microservice for a filter
+        </li>
+        <li>
+          <a target="_blank" href="https://washed.siddharthgelera.com">
+            Washed - Microservice for a filter
         </a>
-      </li>
-      <li>
-        <a target="_blank" href="https://rmnd-r.siddharthgelera.com">
-          rmnd-r - Minimal Tasks List
+        </li>
+        <li>
+          <a target="_blank" href="https://rmnd-r.siddharthgelera.com">
+            rmnd-r - Minimal Tasks List
         </a>
-      </li>
-      <li>
-        <a target="_blank" href="https://github.com/barelyhuman/aForEach">
-          aForEach - Asynchronous ForEach Util for JS
+        </li>
+        <li>
+          <a target="_blank" href="https://github.com/barelyhuman/aForEach">
+            aForEach - Asynchronous ForEach Util for JS
         </a>
-      </li>
-      <li>
-        <a target="_blank" href="https://github.com/barelyhuman/sautocomplete-angularjs">
-          sAutoComplete - AngularJS1.6 Directive for Autocomplete
+        </li>
+        <li>
+          <a target="_blank" href="https://github.com/barelyhuman/sautocomplete-angularjs">
+            sAutoComplete - AngularJS1.6 Directive for Autocomplete
         </a>
-      </li>
-      <li>
-        <a target="_blank" href="https://github.com/barelyhuman/archlinux.sh">
-          archlinux.sh - Script for setup for archlinux
+        </li>
+        <li>
+          <a target="_blank" href="https://github.com/barelyhuman/archlinux.sh">
+            archlinux.sh - Script for setup for archlinux
         </a>
-      </li>
-    </ul>
+        </li>
+      </ul>
 
-    <button className="margin-top-sm  align-start button back-button" onClick={()=>Router.back()}>
-      Back
+      <button className="margin-top-sm  align-start button back-button" onClick={() => Router.back()}>
+        Back
     </button>
-  </div>
+    </div>
   </Layout>
 }
 
