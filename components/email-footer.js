e176fa2e2be20b0b7c26d77ed426008817934971
sidgroks@gmail.com <sidgroks@gmail.com>
2020-09-02 14:46:35 +0530

    typo

--- components/email-footer.js
+++ components/email-footer.js
@@ -7,7 +7,7 @@ export default function EmailFooter() {
       <Spacer y={2} x={1} inline />
       Have something you want me to write about or have a comment on one of my
       posts? You can send an email to&nbsp;
-      <a href="mailto:ahoy@barelyhuman.dev">ahoy@barleyhuman.dev</a>
+      <a href="mailto:ahoy@barelyhuman.dev">ahoy@barelyhuman.dev</a>
       <Spacer y={2} x={1} />
     </p>
   );
