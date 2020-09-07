7cefdfff047f2305171c4124b67213cc26c238fe
Siddharth Gelera <sidgroks@gmail.com>
2020-09-07 09:44:29 +0530

    additional padding for longer lists

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,10 @@
-import Head from 'components/head'
-import Layout from 'components/Layout'
-import Spacer from 'components/Spacer'
-import getPosts from 'lib/get-posts'
-import Link from 'next/link'
-import Router from 'next/router'
+import Head from 'components/head';
+import Layout from 'components/Layout';
+import Spacer from 'components/Spacer';
+import getPosts from 'lib/get-posts';
+import Link from 'next/link';
+import Router from 'next/router';
+import Padding from 'components/padding';
 
 const Blog = ({ posts }) => {
   return (
@@ -12,39 +13,41 @@ const Blog = ({ posts }) => {
         <title>Blog | Reaper</title>
       </Head>
       <Layout>
-        <div className='container'>
-          <ul className='card-border min-width-150-px'>
-            {posts.map(({ meta }, index) => {
-              return (
-                <li key={index}>
-                  <Link href={`blog/${meta.slug}`}>
-                    <a>{meta.title}</a>
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-          <Spacer y={1} />
-          <button
-            className='margin-top-sm  align-start button black outline-btn'
-            onClick={() => Router.push('/')}
-          >
-            Back
-          </button>
-          <Spacer y={2} />
-        </div>
+        <Padding y={3}>
+          <div className="container">
+            <ul className="card-border min-width-150-px">
+              {posts.map(({ meta }, index) => {
+                return (
+                  <li key={index}>
+                    <Link href={`blog/${meta.slug}`}>
+                      <a>{meta.title}</a>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+            <Spacer y={1} />
+            <button
+              className="margin-top-sm  align-start button black outline-btn"
+              onClick={() => Router.push('/')}
+            >
+              Back
+            </button>
+            <Spacer y={2} />
+          </div>
+        </Padding>
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export async function getStaticProps () {
-  const posts = await getPosts()
+export async function getStaticProps() {
+  const posts = await getPosts();
   return {
     props: {
-      posts
-    }
-  }
+      posts,
+    },
+  };
 }
 
-export default Blog
+export default Blog;
