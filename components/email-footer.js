935a79f7644c9cfe55679701fbbf941449435545
Siddharth Gelera <sidgroks@gmail.com>
2020-09-07 10:16:42 +0530

    Proper padding for the email footer

--- components/email-footer.js
+++ components/email-footer.js
@@ -1,14 +1,13 @@
-import Spacer from './Spacer'
+import Padding from './padding';
 
-export default function EmailFooter () {
+export default function EmailFooter() {
   return (
-    <p className='card-border'>
-      <Spacer y={2} />
-      <Spacer y={2} x={1} inline />
-      Have something you want me to write about or have a comment on one of my
-      posts? You can send an email to&nbsp;
-      <a href='mailto:ahoy@barelyhuman.dev'>ahoy@barelyhuman.dev</a>
-      <Spacer y={2} x={1} />
+    <p className="card-border">
+      <Padding all={2}>
+        Have something you want me to write about or have a comment on one of my
+        posts? You can send an email to&nbsp;
+        <a href="mailto:ahoy@barelyhuman.dev">ahoy@barelyhuman.dev</a>
+      </Padding>
     </p>
-  )
+  );
 }
