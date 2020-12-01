9c91a6990fdb857fb413825c0bbfb31c57fb9727
Siddharth Gelera <sidgroks@gmail.com>
2020-12-02 00:43:21 +0530

    add background timer to work list

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,100 +1,108 @@
-import Router from 'next/router'
-import Layout from '../../components/Layout'
-import Spacer from '../../components/Spacer'
+import Router from 'next/router';
+import Layout from '../../components/Layout';
+import Spacer from '../../components/Spacer';
 
-function Work () {
+function Work() {
   return (
     <Layout>
-      <div className='container'>
-        <ul className='card-border min-width-150-px'>
+      <div className="container">
+        <ul className="card-border min-width-150-px">
           <li>
-            <a target='_blank' href='https://mark.reaper.im'>
+            <a
+              target="_blank"
+              href="https://github.com/barelyhuman/background-timer"
+            >
+              Background Timer - React Hook for background timers (OTP,etc)
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://mark.reaper.im">
               Mark - Markdown Editor
             </a>
           </li>
           <li>
-            <a target='_blank' href='https://colors.reaper.im'>
+            <a target="_blank" href="https://colors.reaper.im">
               Colors - Random Color Generator
             </a>
           </li>
           <li>
-            <a target='_blank' href='https://pending.reaper.im'>
+            <a target="_blank" href="https://pending.reaper.im">
               Pending - Kanban Board
             </a>
           </li>
           <li>
-            <a target='_blank' href='http://snips.reaper.im/'>
+            <a target="_blank" href="http://snips.reaper.im/">
               Snips - React Component Snippets
             </a>
           </li>
           <li>
-            <a target='_blank' href='https://hen.reaper.im'>
+            <a target="_blank" href="https://hen.reaper.im">
               Hen - UI Component Playground
             </a>
           </li>
           <li>
-            <a target='_blank' href='https://github.com/barelyhuman/ftrouter/'>
+            <a target="_blank" href="https://github.com/barelyhuman/ftrouter/">
               ftrouter - File Tree based API Routes for NodeJs
             </a>
           </li>
           <li>
-            <a target='_blank' href='https://mailer.reaper.im'>
+            <a target="_blank" href="https://mailer.reaper.im">
               Mailer - Nodemailer as a service
             </a>
           </li>
           <li>
-            <a target='_blank' href='https://tillwhen.barelyhuman.dev/'>
+            <a target="_blank" href="https://tillwhen.barelyhuman.dev/">
               TillWhen - Project based Time tracker
             </a>
           </li>
 
           <li>
-            <a target='_blank' href='https://mnmlcss.js.org/'>
+            <a target="_blank" href="https://mnmlcss.js.org/">
               Mnml.css - Minimal CSS Resets
             </a>
           </li>
           <li>
-            <a target='_blank' href='https://hireme.barelyhuman.dev/'>
+            <a target="_blank" href="https://hireme.barelyhuman.dev/">
               HireMe - Job Network
             </a>
           </li>
           <li>
-            <a target='_blank' href='https://corona.siddharthgelera.com'>
+            <a target="_blank" href="https://corona.siddharthgelera.com">
               Covid - 19 Tracker
             </a>
           </li>
           <li>
-            <a target='_blank' href='https://music.reaper.im'>
+            <a target="_blank" href="https://music.reaper.im">
               Music - Minimal Music Player
             </a>
           </li>
           <li>
-            <a target='_blank' href='https://washed.siddharthgelera.com'>
+            <a target="_blank" href="https://washed.siddharthgelera.com">
               Washed - Microservice for a filter
             </a>
           </li>
           <li>
-            <a target='_blank' href='https://rmnd-r.siddharthgelera.com'>
+            <a target="_blank" href="https://rmnd-r.siddharthgelera.com">
               rmnd-r - Minimal Tasks List
             </a>
           </li>
           <li>
-            <a target='_blank' href='https://github.com/barelyhuman/aForEach'>
+            <a target="_blank" href="https://github.com/barelyhuman/aForEach">
               aForEach - Asynchronous ForEach Util for JS
             </a>
           </li>
           <li>
             <a
-              target='_blank'
-              href='https://github.com/barelyhuman/sautocomplete-angularjs'
+              target="_blank"
+              href="https://github.com/barelyhuman/sautocomplete-angularjs"
             >
               sAutoComplete - AngularJS1.6 Directive for Autocomplete
             </a>
           </li>
           <li>
             <a
-              target='_blank'
-              href='https://github.com/barelyhuman/archlinux.sh'
+              target="_blank"
+              href="https://github.com/barelyhuman/archlinux.sh"
             >
               archlinux.sh - Script for setup for archlinux
             </a>
@@ -102,7 +110,7 @@ function Work () {
         </ul>
         <Spacer y={1} />
         <button
-          className='margin-top-sm  align-start button black outline-btn'
+          className="margin-top-sm  align-start button black outline-btn"
           onClick={() => Router.push('/')}
         >
           Back
@@ -110,7 +118,7 @@ function Work () {
         <Spacer y={2} />
       </div>
     </Layout>
-  )
+  );
 }
 
-export default Work
+export default Work;
