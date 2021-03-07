084a8882283372b46d6572a71999a9e7c2276296
barelyhuman <barelyhuman@users.noreply.github.com>
2021-03-07 16:04:32 +0000

    :robot: Formatted!

--- components/utterance-comments.js
+++ components/utterance-comments.js
@@ -1,27 +1,27 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react'
 
 export const UtterancesComments = () => {
-  const elemRef = useRef();
+  const elemRef = useRef()
 
   useEffect(() => {
     if (!elemRef || !elemRef.current) {
-      return;
+      return
     }
 
-    const scriptElem = document.createElement('script');
-    scriptElem.src = 'https://utteranc.es/client.js';
-    scriptElem.async = true;
-    scriptElem.crossOrigin = 'anonymous';
-    scriptElem.setAttribute('repo', 'barelyhuman/reaper.im');
-    scriptElem.setAttribute('issue-term', 'og:title');
-    scriptElem.setAttribute('label', 'comments-blog');
-    scriptElem.setAttribute('theme', 'github-light');
-    elemRef.current.appendChild(scriptElem);
-  }, [elemRef]);
+    const scriptElem = document.createElement('script')
+    scriptElem.src = 'https://utteranc.es/client.js'
+    scriptElem.async = true
+    scriptElem.crossOrigin = 'anonymous'
+    scriptElem.setAttribute('repo', 'barelyhuman/reaper.im')
+    scriptElem.setAttribute('issue-term', 'og:title')
+    scriptElem.setAttribute('label', 'comments-blog')
+    scriptElem.setAttribute('theme', 'github-light')
+    elemRef.current.appendChild(scriptElem)
+  }, [elemRef])
 
   return (
     <>
       <section ref={elemRef} />
     </>
-  );
-};
+  )
+}
