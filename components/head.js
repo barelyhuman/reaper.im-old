374d1bdff565542deb78272568671cd4d8688ee8
barelyhuman <barelyhuman@users.noreply.github.com>
2021-03-19 21:21:28 +0000

    change favicon to work with rss

--- components/head.js
+++ components/head.js
@@ -1,23 +1,23 @@
-import NextHead from 'next/head'
+import NextHead from 'next/head';
 
 const Head = ({ children }) => {
   return (
     <>
       <NextHead>
         <link
-          href='https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap"
+          rel="stylesheet"
         />
         <link
-          href='https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700&display=swap"
+          rel="stylesheet"
         />
 
-        <link rel='icon' href='https://reaper.im/logo.svg' />
+        <link rel="icon" href="https://reaper.im/favicon.ico" />
         {children}
       </NextHead>
     </>
-  )
-}
+  );
+};
 
-export default Head
+export default Head;
